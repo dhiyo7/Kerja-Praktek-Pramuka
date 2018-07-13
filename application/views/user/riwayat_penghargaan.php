cebc6ec265e03ed3cc5105f638a365b980053ba0
alibudi <35588270+alibudi@users.noreply.github.com>
2018-07-13 16:15:51 +0700

    versi 3
    alibudi

--- application/views/user/riwayat_penghargaan.php
+++ application/views/user/riwayat_penghargaan.php
@@ -6,7 +6,7 @@
                  <div class="panel box-shadow-none text-left content-header">
                       <div class="panel-body" style="padding-bottom:0px;">
                         <div class="col-md-12">
-                            <h3 class="animated fadeInLeft">Penghargaan</h3>
+                            <h3 class="animated fadeInLeft">Riwayat Penghargaan</h3>
                             <br><br>
                         </div>
                       </div>
@@ -20,55 +20,33 @@
                                     <div class="panel-body">
                                       <div class="col-md-6">
                                         <div class="alert alert-danger alert-border alert-dismissible fade in" role="alert">
-                                          <h3>Heading
+                                          <h3>Gambar Penghargaan
+                                          <?php 
+                                            foreach($penghargaan as $d)                  
+                                              ?>
                                             <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
                                           </h3>
-                                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                                          <p><img style="width: 100px;" src='<?= base_url().'uploads/'.$d->gambar ?>'></p>
+                                      <!--    <img style="width: 300px;" src="<?php echo base_url()?>uploads/13.png" alt=""> -->
                                         </div>
                                       </div>
 
                                       <div class="col-md-6">
                                         <div class="alert alert-warning alert-border alert-dismissible fade in" role="alert">
-                                         <h3>Heading
+                                         <h3>Nama
                                           <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
                                         </h3>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                                          
+                                        <p><td><?php echo $d->nama_penghargaan?></td></p>
                                       </div>
                                     </div>
-
+                                   
                                     <div class="col-md-6">
                                       <div class="alert alert-primary alert-border alert-dismissible fade in" role="alert">
-                                       <h3>Heading
+                                       <h3>Keterangan
                                         <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
                                       </h3>
-                                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                    </div>
-                                  </div>
-
-                                  <div class="col-md-6">
-                                    <div class="alert alert-success alert-border alert-dismissible fade in" role="alert">
-                                      <h3>Heading
-                                        <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                      </h3>
-                                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                    </div>
-                                  </div>
-
-                                  <div class="col-md-6">
-                                    <div class="alert alert-info alert-border alert-dismissible fade in" role="alert">
-                                      <h3>Heading
-                                        <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                      </h3>
-                                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                                    </div>
-                                  </div>
-
-                                  <div class="col-md-6">
-                                    <div class="alert alert-default alert-border alert-dismissible fade in" role="alert">
-                                      <h3>Heading
-                                        <button type="button" class="close pull-right" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                      </h3>
-                                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                                      <p><td><?php echo $d->keterangan?></td></p>
                                     </div>
                                   </div>
                                 </div>
@@ -77,35 +55,49 @@
                         </div>
                       </div>
 
+                        <?php if($this->session->flashdata('info')){ ?>
+                <div class="alert alert-warning alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <?php echo $this->session->flashdata('info'); ?>
+                </div>
+            <?php } ?>
+
+            <?php
+                $name = array(
+                    'name'=>'addData',
+                    'class'=>'form-horizontal'
+                    );  
+                echo form_open_multipart('user/addPenghargaan/',$name);
+            ?>
                       <div class="col-md-12 modal-example">
 
               <div class="col-md-12">
                   <div class="panel">
                     <div class="panel-heading">
-                      <h4>Form Upload Penghargaan</h4>
+                      <h4>Form Upload </h4>
                     </div>
                     <div class="panel-body">
 
-                          <div class="col-md-12">
-                              <div class="modal fade">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                      <h4 class="modal-title">Upload</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                      <p>One fine body&hellip;</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                      <button type="button" class="btn btn-primary">Upload</button>
-                                    </div>
-                                  </div><!-- /.modal-content -->
-                                </div><!-- /.modal-dialog -->
-                              </div><!-- /.modal -->
+                       
+                           <div class="form-group">
+                            <label class="col-md-2 control-label text-right">Upload</label>
+                              <div class="col-md-4">
+                              <input type="file" name="gambar" class="form-control">
+                              </div>
+                            </div> <!-- /.modal -->
                           </div>
+                             <div class="form-group">
 
-                    </div>
+                            <label class="col-md-2 control-label text-right"></label>
+                               <div class="col-md-4">
+                            <button  name="submit" value="submit" class="btn ripple-infinite btn-gradient btn-info">
+                                   <div>
+                                      <span>Save </span>
+                                   </div>
+                            </button>
+                          </div>
+                          </div>
+                    </div>  
                   </div>
               </div>
           </div>
