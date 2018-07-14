c1797beaf277e1acd329911c1c25ad23da8348e8
alibudi <35588270+alibudi@users.noreply.github.com>
2018-07-14 15:21:13 +0700

    versi 3
    alibudi

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,38 +2,43 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login extends CI_Controller{
-  function __construct() {
+
+     function __construct() {
 
     parent::__construct();
-    $this->load->model('modelLogin');
-
- }
-  public function index(){
-    $this->load->view('login');
-  }
-    function ceklogin(){
-    if(isset($_POST['login'])){
-      $user = $this->input->post('user',true);
-      $pass = $this->input->post('pass',true);
-      $cek = $this->modelLogin->proseslogin($user, $pass);
-      $hasil = count($cek);
-      if($hasil > 0){
-        $yglogin = $this->db->get_where('tb_anggota',array('username'=>$user, 'password' => $pass))->row();
-        $data = array('udhmasuk' => true,
-            'nama' => $yglogin->nama, 'email' => $yglogin->email, 'username' => $yglogin->username);
-        $this->session->set_userdata($data);
-        if($yglogin->level == 'admin'){
-          redirect('admin/dashboard');
-        }elseif($yglogin->level == 'user'){
-          redirect('user');
-        }
-      }else{
-        redirect('index');
-      }
+    $this->load->model('mod_login');
+}
+    public function index(){
+        $this->load->view('login');
     }
-  }
-  function keluar(){
-    $this->session->sess_destroy();
-    redirect('index');
-  }
+
+    public function proseslogin(){
+        $this->form_validation->set_rules('username', 'username', 'required|trim|xss_clean');
+        $this->form_validation->set_rules('password', 'password', 'required|trim|xss_clean');
+        $user = $this->input->post('username',true);
+        $pass = $this->input->post('password',true);
+        $u = $user;
+        $p = md5($pass);
+        $cek = $this->mod_login->ceklogin($u, $p);
+        $tes = count($cek);
+
+        if ($tes > 0) {
+            $loginn = $this->mod_login->ceklogin($u, $p);
+            $level = $loginn->level;
+            $data = array('id_anggota' => $id);
+            $data = array('level' => $level);
+            $this->session->set_userdata($data);
+            if ($level == 'admin') {
+                    redirect('admin');
+            }elseif($level == 'user'){
+                redirect('user');
+            }
+        }else{
+            echo "<script>alert('Gagal login: Cek username, password!');history.go(-1);</script>";
+        }
+    }   
+    function logout(){
+       $this->session->sess_destroy();
+       redirect('login');
+    }
 }
