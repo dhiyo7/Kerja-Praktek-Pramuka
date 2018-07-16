ee74e6d325c2a9e5b9bc5852535fab7de7920a9a
alibudi <35588270+alibudi@users.noreply.github.com>
2018-07-17 05:51:55 +0700

    versi 4
    alibudi

--- application/models/Mod_Login.php
+++ application/models/Mod_Login.php
@@ -8,5 +8,21 @@
         $this->db->where("password", $pass);
         return $this->db->get("tb_anggota")->row();
       }
+/*  	function login($user,$pass){
+  	
+  	$this->db->select('username,password,level');
+  	$this->db->from('tb_anggota');
+  	$this->db->where("username",$user)
+  	$this->db->where("password",$pass)
+  	$this->db->limit(1);
+
+  	$query = $thi->db->get();
+
+  	if ($query->num_rows()==1) {
+  		return $query->result();
+  	}else{
+  		return false;
+  	}
+  	}
+*/
   }
-?>
--- application/models/ModelProfil.php
+++ application/models/ModelProfil.php
@@ -2,26 +2,25 @@
  defined('BASEPATH') OR exit('No direct script access allowed');
   class ModelProfil extends CI_Model {
 
-  	public function getProfil(){
-	$data = $this->db->get('tb_anggota')->result();
-	return $data;
+  	public function getProfil($id_anggota){
+  	$this->db->select('*');
+  	$this->db->from('tb_anggota');
+  	$this->db->where('id_anggota', $id_anggota);
+  $data =	$this->db->get();
+  	return $data->result();
+  	/*$this->db->where('id_anggota');*/
+	
+	/*$data = $this->db->get('tb_anggota')->result();
+	return $data;*/
  }
-  public function getDataByProfil($id){
-<<<<<<< HEAD
- 	$this->db->where('id_anggota',$id);
-=======
- 	$this->db->where('id',$id);
->>>>>>> 8d6572d76750e65ef3a0816a12f0cde6104b4573
+  public function getDataByProfil($id_anggota){
+ 	$this->db->where('id_anggota',$id_anggota);
  	$data = $this->db->get('tb_anggota')->row();
  	return $data;
  }
 
  public function updateProfil($id,$data){
-<<<<<<< HEAD
  	$this->db->where('id_anggota',$id);
-=======
- 	$this->db->where('id',$id);
->>>>>>> 8d6572d76750e65ef3a0816a12f0cde6104b4573
  	$this->db->update('tb_anggota',$data);
  	$cek = $this->db->affected_rows();
  	return $cek >0 ? true : false;
